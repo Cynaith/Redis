ff0ab11ddadccc3a2ab52702eeafea797ad2458f
Cynaith <894324762@qq.com>
2019-12-06 13:39:54 +0800

    update

---
+++ src/main/java/com/ly/redis/DataType/Hash.md
@@ -0,0 +1,11 @@
+## Java操作哈希类型
+
+- **将fiele:value插入Hash表key中:** `jedis.hset("bigcity","big","beijing");`
+- **获取Hash表key中域为feild的value:** `jedis.hget("bigcity","big");`
+- **插入一个map至Hash表key中:** `jedis.hmset("bigCity2",map);`
+- **通过List接收从Hash表key中返回的多个域:** `List<String> list = jedis.hmget("bigCity2","big1","big2");`
+- **删除Hash表key中的多个feild:** `jedis.hdel("bigCity2","big1","big2");`
+- **获取Hash表的长度:** `jedis.hlen("bigcity");`
+- **是否存在Hash表key:** `jedis.exists("bigCity2");`
+- **返回Hash表中所有feilds:** `jedis.hkeys("bigCity2")`
+- **返回Hash表中所有values:** `jedis.hvals("bigCity2")`
---
+++ src/main/java/com/ly/redis/DataType/java/RedisHash.java
@@ -0,0 +1,32 @@
+package com.ly.redis.DataType.java;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RedisHash {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1",6379);
+        jedis.hset("bigcity","big","beijing");
+        jedis.hget("bigcity","big");
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("big1","shanghai");
+        map.put("big2","wuhan");
+        map.put("big3","xiamen");
+        jedis.hmset("bigCity2",map);
+        List<String> list = jedis.hmget("bigCity2","big1","big2");
+//        for(String str:list){
+//            System.out.println(str);
+//        }
+
+        jedis.hdel("bigCity2","big1","big2");
+        System.out.println(jedis.hget("bigCity2","big1"));
+        jedis.hlen("bigcity");
+        jedis.exists("bigCity2");
+        System.out.println(jedis.hkeys("bigCity2"));
+        System.out.println(jedis.hvals("bigCity2"));
+
+    }
+}
